adf3a7f30852ed36a70fb42f4c22a7994cd039e3
1092024 <80467139+1092024@users.noreply.github.com>
2024-04-13 15:00:47 +0800

    Add files via upload

--- js/chinese.js
+++ js/chinese.js
@@ -2,6 +2,10 @@ var frontAni = document.getElementById("frontAni");
 var gotoMenu = document.getElementById("gotoMenu");
 var introduce = document.getElementById("introduce");
 var toTop = document.getElementById("toTop");
+var menu = document.getElementById("menu");
+var menuAni = document.getElementById("menuAni");
+var menuOptions = document.getElementById("menuOptions");
+
 // var btn1 = document.getElementById("btn1");
 // var btn2 = document.getElementById("btn2");
 // var btn3 = document.getElementById("btn3");
@@ -17,16 +21,28 @@ var windowHeight = window.innerHeight;
 var isPC = windowWidth > 992;
 
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+  menu.style.display = "block";
+}
 
 frontAni.addEventListener("ended", () => {
   if (isPC && !isMobile) {
     frontAni.style.opacity = "0.3";
     setTimeout(function () {
       gotoMenu.style.display = "block";
+      menu.style.display = "block";
     }, 2000);
   } else {
     frontAni.style.opacity = "1";
   }
+});
+
+menu.addEventListener("click", () => {
+  gotoMenu.style.display = "none";
+  menuOptions.style.display = "block";
+  document.body.style.overflow = "hidden";
+  menuAni.style.display = "block";
+  menuAni.play();
 });
 
 gotoMenu.addEventListener("click", () => {
